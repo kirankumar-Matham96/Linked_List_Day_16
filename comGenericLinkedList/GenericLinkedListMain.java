12d00eb8d8d7a28a2281d086f1eedc7ac41ef74d
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-10 19:41:20 +0530

    STARTED: Linked List with welcome message

---
+++ comGenericLinkedList/GenericLinkedListMain.java
@@ -0,0 +1,7 @@
+package comGenericLinkedList;
+
+public class GenericLinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked List Simulator!");
+    }
+}
